7c56031ba5a75fea05630e1125f247f9fb800fad
angelina dorasheva <dorasheva.a@gmail.com>
2021-06-13 23:32:23 +0300

    task-3 Реализована функция регистрации пользолвателя

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,29 @@
 const ApiError = require('../common/error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { User, Basket } = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role}, process.env.SECRET_KEY, { expiresIn: '24' })
+}
 
 class UserController {
-  async registration(req, res) {
+  async registration(req, res, next) {
+    const {email, password, role} = req.body
+    if (!email || !password) {
+      return next(ApiError.badRequest('Email or password is not valid'))
+    }
+    const candidate = await User.findOne({where: { email }})
+    if (candidate) {
+      return next(ApiError.badRequest('The user with this email already exists'))
+    }
 
+    const hashPassword= await bcrypt.hash(password, 5)
+    const user = await User.create({ email, role, password: hashPassword })
+    const basket = await Basket.create({ userId: user.id })
+    const token = generateJwt(user.id, email, role)
+
+    return res.json({ token })
   }
 
   async login(req, res) {
